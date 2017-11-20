505e4cec9e203f7901cce40aa5575f205558997e
LTMana <30500660@qq.com>
2017-11-20 18:45:30 +0800

    提交集成测试

--- test/mocha.js
+++ test/mocha.js
@@ -4,7 +4,7 @@ const {add, mul} = require('../src/math')
 describe('#math', () => {
   describe('add()', () => {
     it('should return 5 when 2 + 3', () => {
-      expect(add(2, 3)).to.equal(6)
+      expect(add(2, 3)).to.equal(5)
     })
     it('should return 5 when 2 + 3', () => {
       expect(add(2, -3)).to.equal(-1)
